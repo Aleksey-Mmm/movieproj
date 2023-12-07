fa99d0b57218ff926039a2ea19eed60c32ff1fb8
Alexey_Mmm <alex@proplus.ru>
2023-12-07 10:54:29 +0900

    настроил виртуальное окружение, настройка статики (media)

--- movieproj/urls.py
+++ movieproj/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from movie import views as movieViews
@@ -24,3 +26,5 @@ urlpatterns = [
     path('about/', movieViews.about, name='about'),
     path('signup/', movieViews.signup, name='signup'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
