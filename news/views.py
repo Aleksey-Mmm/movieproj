ad3e70e2446c728352759f39b623fed824b4c293
AlexMmm <alex@proplus.ru>
2023-12-07 21:32:37 +0900

    news model, templates, register in admin

--- news/views.py
+++ news/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import News
 
 
 # Create your views here.
 def news(request):
-    return render(request, 'news.html')
+    newss = News.objects.all().order_by('-date')
+    return render(request, 'news.html',
+                  {'newss': newss})
