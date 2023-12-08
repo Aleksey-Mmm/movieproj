e830e3b48c182307a93cd3360c1153c2d2198b8d
Alexey_Mmm <alex@proplus.ru>
2023-12-08 13:58:12 +0900

    подключил базовый шаблон

--- movieproj/settings.py
+++ movieproj/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'movieproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'movieproj/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
