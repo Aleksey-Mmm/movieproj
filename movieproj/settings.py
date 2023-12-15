f1a3d430f9a1a71c579b34e7604fdea54abdc44a
Alexey_Mmm <alex@proplus.ru>
2023-12-15 11:27:05 +0900

    movie - details

--- movieproj/settings.py
+++ movieproj/settings.py
@@ -126,3 +126,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+    # BASE_DIR / "movieproj/static", / может неправильно работать в определенных контекстах
+    os.path.join(BASE_DIR, 'movieproj/static'),
+]
