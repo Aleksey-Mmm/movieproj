9755a9a2b5ed97946e2f8d8e8e444c2cc3e037f7
Alexey_Mmm <alex@proplus.ru>
2023-12-15 15:08:34 +0900

    обработка ошибки, наследование формы входа

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,26 @@
+from django.contrib.auth import login
+# from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.shortcuts import redirect
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateForm
 
 
 def signup_account(request):
-    return render(request, 'signup_account.html', {'form': UserCreationForm})
+    if request.method == 'GET':
+        return render(request, 'signup_account.html', {'form': CreateForm})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('home')
+            except IntegrityError:
+                return render(request, 'signup_account.html',
+                              {'form': CreateForm,
+                               'error_message': 'Имя уже занято! Выберите другое.'})
+        else:
+            return render(request, 'signup_account.html', {'form': CreateForm,
+                                                           'error_message': 'Passwords do not match!'})
