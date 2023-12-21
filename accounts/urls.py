a5140aa4d843bde0b21b54cb560a366314fc708e
AlexMmm <alex@proplus.ru>
2023-12-21 22:08:39 +0900

    login и часть реализации

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('signupaccount/', views.signup_account, name='signup_account'),
     path('logout/', views.logout_account, name='logout_account'),
+    path('login/', views.login_account, name='login_account'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
-from django.contrib.auth import login, logout
-# from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.shortcuts import redirect
@@ -29,3 +29,17 @@ def signup_account(request):
 def logout_account(request):
     logout(request)
     return redirect('home')
+
+
+def login_account(request):
+    if request.method == 'GET':
+        return render(request, 'login_account.html', 'form: AuthenticationForm')
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'login_account.html', {'form': AuthenticationForm(),
+                                                          'error_message': 'Неверный логин или пароль!'})
+        else:
+            login(request, user)
+            return redirect('home')
+    
