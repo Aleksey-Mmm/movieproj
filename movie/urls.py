5811e1c2b0527226786f1ea10c69fc9f8817338f
AlexMmm <alex@proplus.ru>
2023-12-23 22:02:57 +0900

    ревью записи и все что с ним связано

--- movie/urls.py
+++ movie/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('<int:movie_id>', views.detail, name='detail'),
+    path('<int:movie_id>/create/', views.create_review, name='create_review'),
 ]
--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Movie
 from django.shortcuts import get_object_or_404
+from .forms import ReviewForm
 
 
 # Create your views here.
@@ -29,3 +30,21 @@ def signup(request):
 def detail(request, movie_id):
     movie = get_object_or_404(Movie, pk=movie_id)
     return render(request, 'detail.html', {'movie': movie})
+
+
+def create_review(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    if request.method == 'GET':
+        return render(request, 'create_review.html', {
+            'form': ReviewForm(), 'movie': movie})
+    else:
+        try:
+            form = ReviewForm(request.POST)
+            new_review = form.save(commit=False)
+            new_review.user = request.user
+            new_review.movie = movie
+            new_review.save()
+            return redirect('detail', new_review.movie.id)
+        except ValueError:
+            return render(request, 'create_review.html', {
+                'form': ReviewForm(), 'error': 'Получены некорректные данные.'})
